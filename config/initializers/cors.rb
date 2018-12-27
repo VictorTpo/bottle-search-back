43e7fc1d55f7509ccf1604d265150043d59587dd
VictorTpo <victor.thepaut@gmail.com>
2018-12-27 15:18:25 +0100

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins 'localhost:3001'
     resource '*',
              headers: :any,
-             methods: %w(get post put patch delete options head)
+             methods: %w(get post put patch delete options head),
+             expose: ['Authorization']
   end
 end
