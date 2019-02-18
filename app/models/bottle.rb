0d8fa31d25c9e23133f16395eb3448b181c802f7
Walid <walid.seoudi@gmail.com>
2019-02-18 18:36:41 +0100

    refacto: use arel instead of pure sql

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -1,11 +1,11 @@
-class Bottle < ActiveRecord::Base
+class Bottle < ApplicationRecord
   validates :name,      presence: true
   validates :vintage,   presence: true
   validates :color, inclusion: { in: %w(red white pink), allow_blank: true }
 
   before_save :set_purchased_and_store
 
-  scope :remaining, -> { where('stored > 0') }
+  scope :remaining, -> { where(arel_table[:stored].gt(0)) }
 
   def set_purchased_and_store
     self.purchased  = 1 if purchased.nil?
